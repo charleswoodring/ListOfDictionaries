6b41aa66a520d37c65635f9a288c73c146613937
Charles Woodring <charles.s.woodring@gmail.com>
2019-04-19 09:17:07 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ListOfDictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
